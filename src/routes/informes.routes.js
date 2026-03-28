994ea2dfe24bfa95df6c7ae8713684430baed82c
Johan-jb <johanjavierburgos@gmail.com>
2026-03-28 00:32:16 -0300

    Corregir nombre de función en rutas de informes

--- src/routes/informes.routes.js
+++ src/routes/informes.routes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { generarInformePDF } = require('../controllers/informes.controller');
+const { generarInformeJugador } = require('../controllers/informes.controller');
 const { authMiddleware } = require('../middleware/auth.middleware');
 
 // Todas las rutas de informes requieren autenticación
 router.use(authMiddleware);
 
-router.get('/jugador/:id_jugador/pdf', generarInformePDF);
+router.get('/jugador/:id/pdf', generarInformeJugador);
 
 module.exports = router;
